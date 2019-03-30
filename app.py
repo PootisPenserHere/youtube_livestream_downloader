fd2ac8ea3e61793aefdab1f6a61baeeb59050947
Jose Pabl Domingo Aramburo Sanchez <josepablo.aramburo@gmail.com>
2019-03-30 00:43:18 +0000

    [add] Project structured to work with flask

--- app.py
+++ app.py
@@ -1,104 +1,25 @@
-import requests
-import json
+from flask import Flask
+from flask import jsonify
 from dotenv import load_dotenv
 import os
+from src.Youtube import Youtube
+
+app = Flask(__name__)
+
+# Loading the env properties into the system
+load_dotenv(".env")
+
+app.config['YOUTUBE_API_KEY'] = os.getenv("YOUTUBE_API_KEY")
+app.config['MAX_RESULTS'] = int(os.getenv("MAX_RESULTS_PER_CALL"))
+app.config['MAX_DEPTH'] = int(os.getenv("MAX_DEPTH"))
+
+youtube = Youtube(app.config['YOUTUBE_API_KEY'], app.config['MAX_RESULTS'], app.config['MAX_DEPTH'])
 
 
-class Youtube():
-    def __init__(self):
-        # Loading the env properties into the system
-        load_dotenv(".env")
-
-        # Componenets for the full url
-        self.base_url = "https://www.googleapis.com/youtube/v3/search"
-        self.parts = ["snippet", "id"]
-        self.api_key = os.getenv("YOUTUBE_API_KEY")
-        self.max_results = int(os.getenv("MAX_RESULTS_PER_CALL"))
-        self.max_depth = int(os.getenv("MAX_DEPTH"))
-
-    def _switch_channel(self, channel_id: str) -> str:
-        """
-        Forms a fully working url to the youtube api that may then be used to query the
-        list of available videos in the chosen channel
-
-        :param channel_id: The id of the public channel to which the queries will be directed
-        :type channel_id: str
-        :return: A url to the youtube data api to query the channel videos
-        :rtype: str
-        """
-
-        concatenated_parts = ",".join(self.parts)
-        parts_section = "part=%s" % concatenated_parts
-        key_section = "%s%s" % ("key=", self.api_key)
-        channel_section = "channelId=%s" % channel_id
-
-        return "%s?%s&%s&%s&order=date&maxResults=%s&eventType=live&type=video" % \
-               (self.base_url, key_section, parts_section, channel_section, self.max_results)
-
-    @staticmethod
-    def _fetch_from_api(url, page_token=None):
-        """
-        Retrives the channel information from the youtube data api
-
-        A list of dictionaries containing each of the videos found will be included with
-        the fields of each dictionary being dependant of the queried arguments determined
-        in the self.parts attribute
-
-        NOTE: the api has a max results of 50, if more results are desired it'll need to
-        be called several times passing a token value at "nextPageToken" to work through
-        the different pages
-
-        :param url: The full url to the youtube api including the api key and channel_id
-        :type url: str
-        :param page_token: Optinal argument, used to fetch subsecuent pages of the video list
-        :type page_token: str
-        :return: A list containing the listing of available videos in the channel limited by the max results
-        :rtype: dict
-        """
-
-        if page_token:
-            url = "%s&pageToken=%s" % (url, page_token)
-
-        raw = requests.get(url)
-
-        return json.loads(raw.text)
-
-    def list_available_videos(self, channel_id):
-        """
-        Forms a full list of the videos found in a given channel by calling the youtube data
-        api in a loop to retrieve all of its paginated results
-
-        :param channel_id: The id of the public channel to which the queries will be directed
-        :type channel_id: str
-        :return: A full list of all videos in a given channel
-        :rtype: list
-        """
-
-        url = self._switch_channel(channel_id)
-        current_iteration = self._fetch_from_api(url)
-        all_videos = current_iteration["items"]
-
-        total_results = current_iteration["pageInfo"]["totalResults"]
-
-        if total_results > self.max_results:
-            needed_iterations = total_results / self.max_results
-
-            count = 1
-            while count < needed_iterations and count < self.max_depth:
-                token = current_iteration["nextPageToken"]
-                current_iteration = self._fetch_from_api(url, token)
-
-                all_videos = all_videos + current_iteration["items"]
-
-                count += 1
-
-            return all_videos
-
-        else:
-            return all_videos
+@app.route('/livestream/<channel_id>', methods=['GET'])
+def find_livestreams_in_channel(channel_id):
+    return jsonify({"status": "success", "message": youtube.list_available_videos(channel_id)})
 
 
-youtube = Youtube()
-videos = youtube.list_available_videos("UCLA_DiR1FfKNvjuUpBHmylQ")
-print(json.dumps(videos))
-print(len(videos))
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
