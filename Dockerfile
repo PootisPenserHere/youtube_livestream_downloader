ec4cc6164a2abb87910a3ba6277ef0e48d55bb60
Jose Pabl Domingo Aramburo Sanchez <josepablo.aramburo@gmail.com>
2019-04-03 01:06:20 +0000

    [add] Setting up sqlalchemy

--- Dockerfile
+++ Dockerfile
@@ -3,12 +3,13 @@ FROM python:3.7-alpine
 WORKDIR /usr/src/app
 COPY . .
 
-RUN pip install --no-cache-dir -r requirements.txt
 
-RUN apk add --no-cache --virtual .build-deps tzdata && \
+RUN apk add --no-cache --virtual .build-deps tzdata g++ mariadb-dev build-base mariadb-connector-c-dev && \
 cp /usr/share/zoneinfo/America/Mazatlan /etc/localtime && \
 echo $CONTAINER_TIMEZONE > /etc/timezone && \
 pip install --no-cache-dir -r requirements.txt && \
 apk del .build-deps
+
+RUN apk add --no-cache mariadb-dev
 
 CMD ["gunicorn"  , "-b", "0.0.0.0:5000", "--reload", "app:app"]
--- app.py
+++ app.py
@@ -3,11 +3,37 @@ from flask import jsonify
 from dotenv import load_dotenv
 import os
 from src.Youtube import Youtube
-
-app = Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
 
 # Loading the env properties into the system
 load_dotenv(".env")
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://%s:%s@%s:%i/%s' % (os.getenv("DB_USER"),
+                                                                    os.getenv("MYSQL_ROOT_PASSWORD"),
+                                                                    os.getenv("DB_HOST"),
+                                                                    int(os.getenv("DB_PORT")),
+                                                                    os.getenv("DB_SCHEMA"))
+db = SQLAlchemy(app)
+
+
+class Streams(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+@app.before_first_request
+def initialize_database():
+    db.create_all()
+
 
 app.config['YOUTUBE_API_KEY'] = os.getenv("YOUTUBE_API_KEY")
 app.config['MAX_RESULTS'] = int(os.getenv("MAX_RESULTS_PER_CALL"))
